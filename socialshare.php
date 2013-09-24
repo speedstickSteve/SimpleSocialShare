a8d5154b97725b2d728b9521a0bf3a5006160bf3
Steve Vaughan <andy@steves-mac-pro.lan>
2013-09-24 10:45:35 +0100

    Refinements
    - Fixed style and script links
    - Sorted FaceBook comment box display

--- socialshare.php
+++ socialshare.php
@@ -7,14 +7,31 @@
 	Version: 1.01
 */
 
-$plugins_url = plugins_url();
-
-wp_register_style( 'share-css', $plugins_url . '/socialshare/css/style.css','','', 'screen' );
+wp_register_style( 'share-css', plugins_url( 'css/style.css', __FILE__ ) ,'','', 'screen' );
 wp_enqueue_style( 'share-css' );
 
 add_action( 'wp_footer', 'add_social_scripts', 20 );
 function add_social_scripts() {
-	echo '<script src="' . get_home_url() . '/wp-content/plugins/socialshare/js/socialscripts.js"></script>';
+	echo '<script src="' . plugins_url( 'js/socialscripts.js', __FILE__ ) . '"></script>';
+}
+
+add_shortcode( 'social_share', 'add_social_shortcode' );
+function add_social_shortcode() {
+	$content = '';
+	$content .=
+	  '<div id="fb-root"></div>
+		<div class="social-shares">
+			<div class="share-box fb-share">
+			<fb:like layout="button_count" width="450" show_faces="false" send="false"></fb:like>
+			</div>
+			<div class="share-box tw-share">
+				<a href="https://twitter.com/share" class="twitter-share-button" data-dnt="true">Tweet</a>
+			</div>
+			<div class="share-box gplus-share">
+				<div class="g-plusone" data-size="medium"></div>
+			</div>
+		</div>';
+	return $content;
 }
 
 if ( is_single() ) {
@@ -36,23 +53,5 @@ if ( is_single() ) {
 			</div>';
 		return $content;
 	}
-}
-
-add_shortcode( 'social_share', 'add_social_shortcode' );
-function add_social_shortcode() {
-	$content .=
-	  '<div id="fb-root"></div>
-		<div class="social-shares">
-			<div class="share-box fb-share">
-			<fb:like layout="button_count" width="450" show_faces="false" send="false"></fb:like>
-			</div>
-			<div class="share-box tw-share">
-				<a href="https://twitter.com/share" class="twitter-share-button" data-dnt="true">Tweet</a>
-			</div>
-			<div class="share-box gplus-share">
-				<div class="g-plusone" data-size="medium"></div>
-			</div>
-		</div>';
-	return $content;
 }
 ?>
