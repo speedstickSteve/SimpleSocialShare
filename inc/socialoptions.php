74cdd18bd4c90336341bcf3b86f4ec722ab3b793
Steve Vaughan <jabber@svcreative.co.uk>
2013-09-24 13:13:11 +0100

    Plug settings in

--- inc/socialoptions.php
+++ inc/socialoptions.php
@@ -1,17 +1,74 @@
 <?php
-add_action( 'admin_menu', 'socialshare_menu' );
 
-function socialshare_menu() {
-	add_options_page( 'Social Share Options', 'Social Share', 'manage_options', 'social-share-options', 'socialshare_options' );
+if ( is_admin() ) {
+	add_action( 'admin_menu', 'socialshare_menu' );
+	add_action( 'admin_init', 'register_socialshare_settings' );
 }
 
-function socialshare_option() {
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-	}
-	echo '<div class="wrap">';
-	screen_icon();
-	echo '<h2>Social Share Plugin Options</h2>''
+function register_socialshare_settings() {
+	register_setting( 'socialshare_options_group', 'show_count' );
+	register_setting( 'socialshare_options_group', 'show_facebook' );
+	register_setting( 'socialshare_options_group', 'show_twitter' );
+	register_setting( 'socialshare_options_group', 'show_google' );
+	register_setting( 'socialshare_options_group', 'show_single' );
+}
+
+function socialshare_menu() {
+	add_options_page( 'Social Share Options', 'Social Sharing', 'manage_options', 'social-share-options', 'socialshare_options' );
+}
+
+function socialshare_options() {
+	?>
+	<div class="wrap">
+		<?php screen_icon(); ?>
+		<h2>Social Share Plugin Options</h2>
+
+		<form method="post" action="options.php">
+			<?php
+			settings_fields( 'socialshare_options_group' );
+			do_settings_fields( 'socialshare_options_group', '' );
+			?>
+
+			<table class="form-table">
+				<tbody>
+					<tr valign="top">
+						<th scope="row">
+							<label>Share Stats:</label>
+						</th>
+						<td>
+							<input type="checkbox" value="1" name="show_count" <?php checked( 1 == get_option( 'show_count' ) ); ?>></input>
+							Show count boxes
+						</td>
+					</tr>
+					<tr valign="top">
+						<th scope="row">
+							<label>Social Networks:</label>
+						</th>
+						<td>
+							<input type="checkbox" value="1" name="show_facebook" <?php checked( 1 == get_option( 'show_facebook' ) ); ?>></input>
+							Facebook <br>
+							<input type="checkbox" value="1" name="show_twitter" <?php checked( 1 == get_option( 'show_twitter' ) ); ?>></input>
+							Twitter <br>
+							<input type="checkbox" value="1" name="show_google" <?php checked( 1 == get_option( 'show_google' ) ); ?>></input>
+							Google+ <br>
+						</td>
+					</tr>
+					<tr valign="top">
+						<th scope="row">
+							<label>Display Locations:</label>
+						</th>
+						<td>
+							<input type="checkbox" value="1" name="show_single" <?php checked( 1 == get_option( 'show_single' ) ); ?>></input>
+							Show at the end of single posts
+						</td>
+					</tr>
+				</tbody>
+			</table>
+
+			<?php submit_button(); ?>
+		</form>
+	</div>
+	<?
 }
 
 ?>
--- socialshare.php
+++ socialshare.php
@@ -7,50 +7,110 @@
 	Version: 1.01
 */
 
-wp_register_style( 'share-css', plugins_url( 'css/style.css', __FILE__ ) ,'','', 'screen' );
-wp_enqueue_style( 'share-css' );
+include 'inc/socialoptions.php';
+
+//wp_register_style( 'share-css', plugins_url( 'css/style.css', __FILE__ ) ,'','', 'screen' );
+//wp_enqueue_style( 'share-css' );
 
 add_action( 'wp_footer', 'add_social_scripts', 20 );
 function add_social_scripts() {
 	echo '<script src="' . plugins_url( 'js/socialscripts.js', __FILE__ ) . '"></script>';
 }
 
+add_action( 'wp_head', 'add_social_styles' );
+function add_social_styles() {
+	echo '<link rel="stylesheet" href="' . plugins_url( 'css/style.css', __FILE__ ) . '" />';
+}
+
+function grab_facebook() {
+	$count = get_option( 'show_count' );
+	$class = '';
+	if ( $count == 1 ) {
+		$layout = 'button_count';
+	} 
+	else {
+		$layout = 'standard';
+		$class = 'fb-no-count';
+	}
+	$markup = '';
+	$markup .=
+   '<div class="share-box fb-share ' . $class . '">
+		<fb:like layout="' . $layout . '" show_faces="false" send="false"></fb:like>
+	</div>';
+	return $markup;
+}
+
+function grab_twitter() {
+	$count = get_option( 'show_count' );
+	$class = '';
+	if ( $count == 1 ) {
+		$layout = '';
+	}
+	else {
+		$layout = 'data-count="none"';
+		$class = 'no-count';
+	}
+	$markup = '';
+	$markup .=
+   '<div class="share-box tw-share ' . $class . '">
+		<a href="https://twitter.com/share" class="twitter-share-button" ' . $layout . '></a>
+	</div>';
+	return $markup;
+}
+
+function grab_google() {
+	$count = get_option( 'show_count' );
+	$class = '';
+	if ( $count == 1 ) {
+		$layout = '';
+	}
+	else {
+		$layout = 'data-annotation="none"';
+		$class = 'no-count';
+	}
+	$markup = '';
+	$markup .=
+   '<div class="share-box gplus-share ' . $class . '">
+		<div class="g-plusone" data-size="medium" ' . $layout . '></div>
+	</div>';
+	return $markup;
+}
+
 add_shortcode( 'social_share', 'add_social_shortcode' );
 function add_social_shortcode() {
 	$content = '';
-	$content .=
-	  '<div id="fb-root"></div>
-		<div class="social-shares">
-			<div class="share-box fb-share">
-			<fb:like layout="button_count" width="450" show_faces="false" send="false"></fb:like>
-			</div>
-			<div class="share-box tw-share">
-				<a href="https://twitter.com/share" class="twitter-share-button" data-dnt="true">Tweet</a>
-			</div>
-			<div class="share-box gplus-share">
-				<div class="g-plusone" data-size="medium"></div>
-			</div>
-		</div>';
+	$content .= '<div id="fb-root"></div><div class="social-shares">';
+	if ( get_option( 'show_facebook' ) == 1 ) {
+		$content .= grab_facebook();
+	}
+	if ( get_option( 'show_twitter' ) == 1 ) {
+		$content .= grab_twitter();
+	}
+	if ( get_option( 'show_google' ) == 1 ) {
+		$content .= grab_google();
+	}
+	$content .=	'</div>';
 	return $content;
 }
 
-add_action( 'the_content', 'add_social_buttons' );
-function add_social_buttons( $content ) {
-	if ( is_single() ) {
-		$content .=
-		  '<div id="fb-root"></div>
-			<div class="social-shares">
-				<div class="share-box fb-share">
-				<fb:like layout="button_count" width="450" show_faces="false" send="false"></fb:like>
-				</div>
-				<div class="share-box tw-share">
-					<a href="https://twitter.com/share" class="twitter-share-button" data-dnt="true">Tweet</a>
-				</div>
-				<div class="share-box gplus-share">
-					<div class="g-plusone" data-size="medium"></div>
-				</div>
-			</div>';
+if ( get_option( 'show_single' ) == 1 ) {
+	add_action( 'the_content', 'add_social_buttons' );
+	function add_social_buttons( $content ) {
+		if ( is_single() ) {
+			$content .= '<div id="fb-root"></div><div class="social-shares">';
+			if ( get_option( 'show_facebook' ) == 1 ) {
+				$content .= grab_facebook();
+			}
+			if ( get_option( 'show_twitter' ) == 1 ) {
+				$content .= grab_twitter();
+			}
+			if ( get_option( 'show_google' ) == 1 ) {
+				$content .= grab_google();
+			}
+			$content .=	'</div>';
+			return $content;
+		}
+		return $content;
 	}
-	return $content;
 }
 ?>
